e19bbc3b80a4bf21c2602d2118531c3536891a4f
Karabo Oliphant🧬 <karabooliphant34@gmail.com>
2025-10-29 09:50:07 +0200

    Create Program.cs

---
+++ Program.cs
@@ -0,0 +1,13 @@
+using System;
+
+namespace Revenue
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Revenue Calculator");
+            Console.WriteLine("Welcome to the Revenue application!");
+        }
+    }
+}
